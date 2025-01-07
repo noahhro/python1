fff1575a10a767b2067b9c32ce339dc6eb784c7a
noahhro <120349566+noahhro@users.noreply.github.com>
2025-01-07 10:24:54 +0545

    Add files via upload

---
+++ MADLIBS- A JOURNAL.py
@@ -0,0 +1,9 @@
+HOW=input("HOW WAS YOUR DAY? ")
+WHO=input("WHO DID YOU GO WITH? ")
+WHERE=input("WHERE DID YOU GO? ")
+WHEN=input("WHEN DID YOU REACH THERE? ")
+WHY=input("WHY DID YOU GO THERE? ")
+WHAT=input("WHAT DID YOU DO NEW TODAY? ")
+
+
+print(f"Today was a {HOW} day. I went on a date with {WHO}. we went to {WHERE}, we arrived at {WHEN}.I always wanted to go here with {WHO} because {WHY}. Surprisingly, {WHAT} and i was totally {HOW}.")
